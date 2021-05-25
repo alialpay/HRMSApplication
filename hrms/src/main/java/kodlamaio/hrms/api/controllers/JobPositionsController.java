6d7ee23971d07a24b67c3600eb2bc1fc69cc143b
Ali Alpay <alialpay96@hotmail.com>
2021-05-25 21:40:06 +0300

    controller code refactoring

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
@@ -3,9 +3,7 @@ package kodlamaio.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
 import kodlamaio.hrms.entities.concretes.JobPosition;
