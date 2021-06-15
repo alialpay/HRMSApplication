b6d40728657b0fcc9dc1f3a77cf1b94fec24a54d
Ali Alpay <alialpay96@hotmail.com>
2021-06-15 14:50:39 +0300

    image

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobSeekerPhotosController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobSeekerPhotosController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import kodlamaio.hrms.entities.concretes.JobSeeker;
 import kodlamaio.hrms.business.abstracts.JobSeekerPhotoService;
+import kodlamaio.hrms.entities.concretes.JobSeeker;
 import kodlamaio.hrms.entities.concretes.JobSeekerPhoto;
 
 @RestController
