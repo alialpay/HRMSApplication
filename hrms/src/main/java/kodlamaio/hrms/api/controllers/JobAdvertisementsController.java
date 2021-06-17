4b159c8fbac087a164c07e814f0f7b5d33104ef7
Ali Alpay <alialpay96@hotmail.com>
2021-06-18 00:34:42 +0300

    @CrossOrigin

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import kodlamaio.hrms.entities.concretes.JobAdvertisement;
 
 @RestController
 @RequestMapping("api/jobAdvertisements")
+@CrossOrigin
 public class JobAdvertisementsController {
 
 	private JobAdvertisementService jobAdvertisementService;
